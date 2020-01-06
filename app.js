b5bb7b8e96b0668b5c52586ca2256b8d937a733a
vntruong1999 <56061432+vntruong1999@users.noreply.github.com>
2020-01-06 02:46:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,67 @@
 var budgetController = (function () {
     
+    // Constructor to construct an object for an expense
+    var Expense = function (type, description, amount) {
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+    }
+    
+    // Constructor to construct an object foe an income
+    var Income = function (type, description, amount) {
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+    }
+    
+    // Object to hold data for total income
+    var incomeData = {
+        // Array which will be used to store income objects
+        arrayOfIncome : [],
+        
+        // Variable to hold the total income
+        totalIncome : 0
+    }
+    
+    // Object to hold data for total expense
+    var expenseData = {
+        // Array which will be used to store expense objects
+        arrayOfExpense : [],
+    
+        // Variable to hold the total expense
+        totalExpense : 0
+    }
+    
+    return {
+        addItem: function (type, description, amount) {
+            
+            var newItem, ID;
+            
+            // ID will be the total number of items
+            ID = (incomeData.arrayOfIncome.length + expenseData.arrayOfExpense.length); 
+            
+            if (type == "inc") {
+                // Create a new item for income
+                newItem = new Income(type, description, amount);
+                
+                console.log('New item added');
+                
+                // Add that item to the array of incomes
+                incomeData.arrayOfIncome.push(newItem);
+                
+            } else if (type == "exp") {
+                // Create a new item for expense
+                newItem = new Expense(type, description, amount);
+                
+                // Add that item to the array of expense
+                expenseData.arrayOfExpense.push(newItem);
+            }
+        },
+        
+        showIncomeItem: function () {
+            return incomeData.arrayOfIncome.length;
+        }
+    }
     
 })();
 
@@ -25,7 +87,7 @@ var UIController = (function () {
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
-                  
+    
     // Function to be executed when either add button is clicked or enter key is pressed
     var addFunction = function () {
         // Get the input from the user
@@ -34,7 +96,11 @@ var controller = (function (budgetCtrl, UICtrl) {
         console.log(UICtrl.getUserInputDescription());
         
         console.log(UICtrl.getUserInputValue());
+        
         // Add item to budget controller
+        budgetCtrl.addItem(UICtrl.getUserInputType, UICtrl.getUserInputDescription, UICtrl.getUserInputValue);
+        
+        console.log(budgetCtrl.showIncomeItem());
         
         // Add item to the UI
         
@@ -43,16 +109,32 @@ var controller = (function (budgetCtrl, UICtrl) {
         // Display it
     }
     
-    // Set up the event so that when the add button is clicked, the input from the user will be read
-    document.querySelector(".add__btn").addEventListener('click', addFunction);
+    // Function to set up event listener
+    var setupEventListener = function () {
+        // Set up the event so that when the add button is clicked, the input from the user will be read
+        document.querySelector(".add__btn").addEventListener('click', addFunction);
+
+        // Set up the event so that when the enter key is pressed, the input from the user will be read
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode == 13) {
+
+                addFunction();
+
+            }
+        });
+    }
     
-    // Set up the event so that when the enter key is pressed, the input from the user will be read
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode == 13) {
+    // Return the object that will call the function which will be use to initialize the app
+    return {
+        initialize: function () {
             
-            addFunction();
+            console.log('This is working ');
             
+            setupEventListener();
         }
-    });
+    }
     
 })(budgetController, UIController);
+
+// Call the initialize function to initialize the app
+controller.initialize();

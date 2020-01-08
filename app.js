ff66535525d9468b6ab8212a7ba872d322a3d4b7
vntruong1999 <56061432+vntruong1999@users.noreply.github.com>
2020-01-07 19:31:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,10 +67,14 @@ var budgetController = (function () {
             return newItem;
         },
         
-        testing: function () {
-            console.log(incomeData);
-
-            console.log(expenseData);
+        // Function which will be used to get the total income
+        getTotalIncome: function () {
+            return Number(incomeData.totalIncome);
+        },
+        
+        // Function which will be used to get the total expense
+        getTotalExpense: function () {
+            return Number(expenseData.totalExpense);
         }
     }
     
@@ -94,6 +98,7 @@ var UIController = (function () {
             return document.querySelector(".add__value").value;
         },
         
+        // Function which will be used to add item to the UI
         addListItem: function (obj, type) {
             
             // HTML string to be added and replaced
@@ -130,25 +135,98 @@ var UIController = (function () {
             else if (type == 'inc') {
                 document.querySelector('.income__list').insertAdjacentHTML('beforeend', newHTML);
             }
-        }
+        },
+        
+        // This function will be called after each time a new item is added in order to remove the component of the previous input
+        clearField: function () {
+            
+            // This one is used to hold the list of input values
+            var fields;
+            
+            // Thí one is used to hold the array of input values
+            var fieldArray;
+            
+            fields = document.querySelectorAll('.add__description' + ', ' + '.add__value');
+            
+            // Convert that field into an array and store it into the fieldArray
+            fieldArray = Array.prototype.slice.call(fields);
+            
+            fieldArray.forEach(function(curent, index, array) {
+                curent.value = "";
+            });
+        },
+        
+        // Function which will be used to update the total income
+        updateTotalIncome: function (totalIncome) {
+            // Get the total income element of the HTML and set it to the current total income value
+            document.querySelector('.budget__income--value').textContent = totalIncome;
+        },
+        
+        // Function which will be used to update the total expense
+        updateTotalExpense: function (totalExpense) {
+            // Get the total expense element of the HLTM and set it to the current total expense value
+            document.querySelector('.budget__expenses--value').textContent = totalExpense;
+        },
+        
+        // Function which will be used to update the current date and time
+        updateCurrentDateAndTime: function (currentDateAndTime) {
+            // Replace the part of the HTML file which will help with displaying date and time
+            document.body.innerHTML = document.body.innerHTML.replace('<span class="budget__title--month">%Month%</span>', '<span class="budget__title--month">' + currentDateAndTime + '</span>')
+        },
+        
+        // Function which will be used to reset the current income and expense to 0 when initializing the app
+        resetIncomeAndExpense: function () {
+            
+            // Set the current income to 0 and display it on the UI
+            document.querySelector('.budget__income--value').textContent = Number(0);
+            
+            // Set the current expense to 0 and display it to the UI
+            document.querySelector('.budget__expenses--value').textContent = Number(0);
+        },
+        
+        // Function which will be used to update the current budget
+        updateBudget: function (inputType, inputValue) {
+            
+            // Variable to store the calculated budget which will also be returned later
+            var calculatedBudget = 0;
+            
+            // Get the current total budget from the UI
+            var totalBudget = Number(document.querySelector('.budget__value').textContent);
+            
+            // Calculate the budget
+            if (inputType == 'inc') {
+                calculatedBudget = totalBudget + Number(inputValue);
+            } else if (inputType == 'exp') {
+                calculatedBudget = totalBudget - Number(inputValue);
+            }
+            
+            // Display the calculated budget
+            document.querySelector('.budget__value').textContent = calculatedBudget;
+            
+            // Return the calculated budget
+            return calculatedBudget;
+        },
+    
     };
     
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
     
-    // Function to be executed when either add button is clicked or enter key is pressed
+    // Function to be executed when either add button is clicked or enter key is pressed to add item to the list and display on the UI
     var addFunction = function () {
         
         // Newly created object
         var newItem;
         
-        // Get the input from the user
-        console.log(UICtrl.getUserInputType());
-        
-        console.log(UICtrl.getUserInputDescription());
-        
-        console.log(UICtrl.getUserInputValue());
+        // Get the input from the user and validate it
+        if (UICtrl.getUserInputDescription() == "" || UICtrl.getUserInputValue() <= 0 || isNaN(Number(UICtrl.getUserInputValue()))) {
+            // Let the user know that the invalid input has been entered
+            alert('The input is invalid. Please make sure that \n 1. The input for description is not blank \n 2. Input value is greater than 0 \n 3. The input for value are all numeric');
+            
+            // Get out of the function
+            return;
+        }
         
         // Add item to budget controller
         newItem = budgetCtrl.addItem(UICtrl.getUserInputType(), UICtrl.getUserInputDescription(), UICtrl.getUserInputValue());
@@ -160,15 +238,28 @@ var controller = (function (budgetCtrl, UICtrl) {
             description: newItem.description
         }, UICtrl.getUserInputType());
         
-        // Calculate the budget
+        // Calculate the budget by calling the update total income and update total expense method from the UIController object
+        // Update the total expense and display it
+        UICtrl.updateTotalExpense(budgetCtrl.getTotalExpense());
         
-        // Display it
+        // Update the total income and display it
+        UICtrl.updateTotalIncome(budgetCtrl.getTotalIncome());
+        
+        // Update the total budget and display it
+        UICtrl.updateBudget(UICtrl.getUserInputType(), UICtrl.getUserInputValue());
+        
+        // Clear the field
+        UICtrl.clearField();
+    }
+    
+    // Function which will be called to remove item from the list
+    var deleteController = function (event) {
+        console.log(event.target);   
+        console.log('Clicked');
     }
     
     // Function to set up event listener
     var setupEventListener = function () {
-        
-        console.log('Event listener has been set ');
         
         // Set up the event so that when the add button is clicked, the input from the user will be read
         document.querySelector(".add__btn").addEventListener('click', addFunction);
@@ -181,15 +272,29 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             }
         });
+        
+        // Function which will be used to delete item from the list
+        document.querySelector('.container').addEventListener('click', deleteController);
     }
     
     // Return the object that will call the function which will be use to initialize the app
     return {
         initialize: function () {
             
-            console.log('This is working ');
-            
+            // Call the function to set up event listener when the app starts
             setupEventListener();
+            
+            // Call the function to set up current date and time
+            var d = new Date();
+            
+            UICtrl.updateCurrentDateAndTime((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear());
+            
+            // Ask the user to enter the available budget for the current day
+            //document.querySelector('.budget__value').textContent = Number(prompt('Enter the available budget for today'));
+            document.querySelector('.budget__value').textContent = Number(3000);
+            
+            // Reset the current income and expense
+            UICtrl.resetIncomeAndExpense();
         }
     }
     
